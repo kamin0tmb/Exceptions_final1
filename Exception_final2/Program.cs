69209dc3160509112b0758bd741c8664096e5024
kamin0 <jm187@yandex.ru>
2022-05-22 19:49:27 +0300

    Добавлено 9.6.2

---
+++ Exception_final2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exception_final2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[] { "Минаев", "Шарикова", "Иванов", "Любимов", "Каримов" };
+            Sort sort = new Sort(array);
+            sort.ChoosenEvent += ShowArray;
+            try
+            {
+               sort.Choose();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введено некорректное значение");
+            }
+
+            void ShowArray(int numb)
+            {
+                switch (numb)
+                {
+                    case 1: sort.SortAZ(); break;
+                    case 2: sort.SortZA(); break;
+                }
+            }
+        }
+
+
+    }
+} 
+
---
+++ Exception_final2/Sort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exception_final2
+{
+    internal class Sort
+    {
+        string[] array;
+
+            public Sort(string[] array)
+            {
+                this.array = array;
+            }
+
+            public delegate void SortDelegate(int numb);
+            public event SortDelegate ChoosenEvent;
+            protected void Choosen(int numb)
+            {
+                ChoosenEvent?.Invoke(numb);
+            }
+            public void Choose()
+            {
+                Console.WriteLine("\nСортировка фамилий: \n\t 1 - по возрастанию (от А до Я) \n\t 2 - по убыванию (от Я до А)");
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number != 1 && number != 2) throw new FormatException();
+                Choosen(number);
+            }
+            public void SortAZ()
+            {
+                Console.WriteLine("по возрастанию:");
+                Array.Sort(array);
+                foreach (string str in array)
+                    Console.WriteLine(str);
+            }
+            public void SortZA()
+            {
+                Console.WriteLine("по убыванию:");
+                Array.Sort(array);
+                Array.Reverse(array);
+                foreach (string str in array)
+                    Console.WriteLine(str);
+            }
+     }
+}
