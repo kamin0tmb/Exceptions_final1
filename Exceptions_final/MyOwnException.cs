bd84730904468e3c2b8cb75304a52ac929c1841c
kamin0 <jm187@yandex.ru>
2022-05-22 18:17:48 +0300

    Добавьте файлы проекта.

---
+++ Exceptions_final/MyOwnException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Exceptions_final
+{
+    [Serializable]
+    internal class MyOwnException : Exception
+    {
+        public MyOwnException()
+        {
+        }
+        public MyOwnException(string message) : base(message)
+        {
+        }
+
+    }
+}
---
+++ Exceptions_final/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Exceptions_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Exception> exceptions = new List<Exception>();
+            exceptions.Add(new MyOwnException("Что-то пошло не по плану и мы уже выясняем причину. Вам придется немного подождать."));
+            exceptions.Add(new ArgumentException());
+            exceptions.Add(new DivideByZeroException());
+            exceptions.Add(new FileNotFoundException());
+            exceptions.Add(new FormatException());
+            foreach (Exception ex in exceptions)
+            {
+                try
+                {
+                    throw ex;
+                }
+                catch (Exception e) when (e is MyException)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (Exception e) when (e is ArgumentException)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (Exception e) when (e is DivideByZeroException)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (Exception e) when (e is FileNotFoundException)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (Exception e) when (e is FormatException)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                Console.WriteLine("Enter, чтобы продолжить");
+                continue;
+            }
+        }
+    }
+}
